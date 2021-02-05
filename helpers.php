224772c4c54f8be23c20d133bdf18d577ed9f575
Jorge P. Hernandez Lalcebo <j.lalcebo@chargebacks911.com>
2021-02-05 13:55:34 -0500

    Add helpers.php

---
+++ helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+//if (!function_exists('exampleHelperFunction')) {
+//    /**
+//     * @param  string  $argOne
+//     * @param  int  $argTwo
+//     * @return string
+//     */
+//    function exampleHelperFunction(string $argOne, int $argTwo): string
+//    {
+//        return 'Example Helper Function: ' . $argOne . ', ' . ($argTwo + 100);
+//    }
+//}
